ae138d9050c0df02c79241382d339c884c01d37b
Robert Muthuri <muthuri.r@gmail.com>
2019-10-07 22:57:28 +0300

    Adds a passing test to check the toast displays the correct content.

--- app/src/androidTest/java/com/example/referendum_chat/OneOnOneActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/example/referendum_chat/OneOnOneActivityInstrumentationTest.java
@@ -1,11 +1,44 @@
 package com.example.referendum_chat;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.view.View;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.Matchers.not;
+
 
 @RunWith(AndroidJUnit4.class)
 public class OneOnOneActivityInstrumentationTest {
 
+    @Rule
+    public ActivityTestRule<OneOOneActivity> activityTestRule = new ActivityTestRule<>(OneOOneActivity.class);
 
+    @Test
+    public void listItemClickDisplaysToastWuthCorrectContent() {
+        View activityDecorView = activityTestRule.getActivity().getWindow().getDecorView();
+
+        String referendumDefinition = "Definition: a general vote by the electorate on a single political question which has been referred to them for a direct decision.";
+
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+
+        onView(withText(referendumDefinition)).inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(referendumDefinition)));
+
+    }
 }
