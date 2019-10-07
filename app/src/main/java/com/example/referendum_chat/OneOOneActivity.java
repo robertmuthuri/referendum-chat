ba85c347defe9888068b293be57edc979fb5a9ce
Robert Muthuri <muthuri.r@gmail.com>
2019-10-07 16:13:47 +0300

    Binds the list view to the 101 layout and creates an array adapter to display the layout content.

--- app/src/main/java/com/example/referendum_chat/OneOOneActivity.java
+++ app/src/main/java/com/example/referendum_chat/OneOOneActivity.java
@@ -3,13 +3,23 @@ package com.example.referendum_chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class OneOOneActivity extends AppCompatActivity {
+    @BindView(R.id.listView) ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_oone);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, basics);
+        mListView.setAdapter(adapter);
     }
 
 
