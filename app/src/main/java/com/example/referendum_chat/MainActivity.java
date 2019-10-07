65c7b93e0dd7feaf1fcd0eb74e675f84e84eb8c1
Robert Muthuri <muthuri.r@gmail.com>
2019-10-07 12:17:30 +0300

    Implements the View.onClickListener interface to DRY code.

--- app/src/main/java/com/example/referendum_chat/MainActivity.java
+++ app/src/main/java/com/example/referendum_chat/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.referendum_chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.oneOOneButton) Button mFindOneOOneButton;
     @BindView(R.id.findResourceButton) Button mFindResourceButton;
     @BindView(R.id.findRefeButton) Button mFindRefeButton;
@@ -18,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mFindOneOOneButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == mFindOneOOneButton) {
+         //
+        }
     }
 }
