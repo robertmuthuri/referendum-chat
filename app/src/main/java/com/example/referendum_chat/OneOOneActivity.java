405db2cddc536eaf3285479256651be69850cda9
Robert Muthuri <muthuri.r@gmail.com>
2019-10-11 15:57:59 +0300

    Updates gitignore file.

--- app/src/main/java/com/example/referendum_chat/OneOOneActivity.java
+++ app/src/main/java/com/example/referendum_chat/OneOOneActivity.java
@@ -6,15 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class OneOOneActivity extends AppCompatActivity {
     @BindView(R.id.listView) ListView mListView;
+    @BindView(R.id.imageView) ImageView mImageView;
 
     private String[] basics = new String[]{ "A referendum is general vote by the electorate on a single political question which has been referred to them for a direct decision.\n", "The supremacy of the Constitution\n\nThe territory of Kenya \n\nThe sovereignty of the people\n\nThe national values and principles of governance\n\nThe bill of rights\n\nThe term of office of the President\n\nThe independence of the Judiciary, the commissions, and independent offices in the Constitution\n\nThe objects, principles and structure of devolved government, and Amendment of the Constitution \n", "a) A bill introduced in either House of Parliament\n\nb) A popular initiative signed by at least one million registered voters.\n", "\na) At least 20% of the registered voters in at least half of the counties vote in the referendum, and\n\nb) The amendment is supported by a simple majority of the citizens voting in a referendum\n"};
 
@@ -25,6 +29,7 @@ public class OneOOneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_oone);
         ButterKnife.bind(this);
+//        Picasso.with(this).load(R.drawable.kenya).into(mImageView);
 
 //        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, basics);
         MyOneOOneArrayAdapter adapter = new MyOneOOneArrayAdapter(this, android.R.layout.simple_list_item_1, headers, basics);
