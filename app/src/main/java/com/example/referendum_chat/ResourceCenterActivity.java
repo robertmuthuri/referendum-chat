08261470ee2438f8b2c24f1385772a07d4f6cd4c
Robert Muthuri <muthuri.r@gmail.com>
2019-10-17 11:32:58 +0300

    Adds an array-adapter to the spinner in the resource-center activity.

--- app/src/main/java/com/example/referendum_chat/ResourceCenterActivity.java
+++ app/src/main/java/com/example/referendum_chat/ResourceCenterActivity.java
@@ -3,12 +3,27 @@ package com.example.referendum_chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ResourceCenterActivity extends AppCompatActivity {
+    @BindView(R.id.resourceSpinner) Spinner mResourceSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resource_center);
+        ButterKnife.bind(this);
+
+    // Create an ArrayAdapter using the string array and a default spinner layout.
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.kenyan_towns_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        mResourceSpinner.setAdapter(adapter);
     }
 }
