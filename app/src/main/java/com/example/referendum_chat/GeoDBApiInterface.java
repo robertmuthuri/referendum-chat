4087af77b591d4fb059d07df80b23b000fc6b235
Robert Muthuri <muthuri.r@gmail.com>
2019-10-17 16:27:30 +0300

    Adds GeoDB API interface class to facilitate API calls.

---
+++ app/src/main/java/com/example/referendum_chat/GeoDBApiInterface.java
@@ -0,0 +1,14 @@
+package com.example.referendum_chat;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface GeoDBApiInterface {
+    @GET("geo/cities")
+    Call<GeoDBCitiesSearchResponse> getCities(
+            @Query("country") String country,
+            @Query("term") String term
+    );
+
+}
