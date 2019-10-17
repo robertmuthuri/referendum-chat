6e9c228ef3141087bbbcda2b7ca79f08643caabe
Robert Muthuri <muthuri.r@gmail.com>
2019-10-17 22:36:55 +0300

    Adds the call-enqueue method on the resource center activity.

--- app/src/main/java/com/example/referendum_chat/GeoDBApiInterface.java
+++ app/src/main/java/com/example/referendum_chat/GeoDBApiInterface.java
@@ -5,10 +5,11 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface GeoDBApiInterface {
-    @GET("geo/cities")
+    @GET("geo/cities?countryIds=ke&limit=10&minPopulation=65000")
+
     Call<GeoDBCitiesSearchResponse> getCities(
-//            @Query("country") String country,
-//            @Query("term") String term
+            @Query("countryIds") String countryIds,
+            @Query("term") String term
     );
 
 }
--- app/src/main/java/com/example/referendum_chat/ResourceCenterActivity.java
+++ app/src/main/java/com/example/referendum_chat/ResourceCenterActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ResourceCenterActivity extends AppCompatActivity {
     @BindView(R.id.resourceSpinner) Spinner mResourceSpinner;
+    @BindView(R.id.kenyanCitiesListView) ListView mKenyanCitiesListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +31,32 @@ public class ResourceCenterActivity extends AppCompatActivity {
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        mResourceSpinner.setAdapter(adapter);
+//        mResourceSpinner.setAdapter(adapter);
 
         GeoDBApiInterface client = GeoDBClient.getClient();
+        String country = "countryIds";
+        Call<GeoDBCitiesSearchResponse> call = client.getCities(country, "ke");
 
-//        Call<GeoDBCitiesSearchResponse> call = client.getCities(country, "term");
+        call.enqueue(new Callback<GeoDBCitiesSearchResponse>() {
+            @Override
+            public void onResponse(Call<GeoDBCitiesSearchResponse> call, Response<GeoDBCitiesSearchResponse> response) {
+                if(response.isSuccessful()) {
+                    List<Datum> datumList = response.body().getData();
+                    String[] cities = new String[datumList.size()];
 
+                    for (int i = 0; i < cities.length; i++) {
+                        cities[i] = datumList.get(i).getName();
+                    }
+                    ArrayAdapter citiesAdapter = new ArrayAdapter(ResourceCenterActivity.this, android.R.layout.simple_list_item_1, cities);
+                    mResourceSpinner.setAdapter(citiesAdapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GeoDBCitiesSearchResponse> call, Throwable t) {
+
+            }
+        });
     }
+
 }
