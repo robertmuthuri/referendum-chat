7320947456080528d8aa67b9284496e06b306612
Robert Muthuri <muthuri.r@gmail.com>
2019-10-17 10:53:02 +0300

    Adds a resouce-center-activity and corresponding layout.

---
+++ app/src/main/java/com/example/referendum_chat/ResourceCenterActivity.java
@@ -0,0 +1,14 @@
+package com.example.referendum_chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResourceCenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resource_center);
+    }
+}
