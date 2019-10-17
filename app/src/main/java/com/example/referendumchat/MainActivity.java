df1ef4fdc4070da37cdb1b9f4e087096f4cd0dc1
Robert Muthuri <muthuri.r@gmail.com>
2019-10-17 10:18:51 +0300

    Resolve conflict in main activity.

--- app/src/main/java/com/example/referendumchat/MainActivity.java
+++ app/src/main/java/com/example/referendumchat/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.referendum_chat.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
