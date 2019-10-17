10415440252bb3a9ce5b5c3524f5ffdaed6c79ad
Robert Muthuri <muthuri.r@gmail.com>
2019-10-17 15:11:25 +0300

    Instruct build gradle to add credentials to the BuildConfig file when it is created.

---
+++ app/src/main/java/com/example/referendum_chat/Constants.java
@@ -0,0 +1,6 @@
+package com.example.referendum_chat;
+
+public class Constants {
+    public static final String GeoDB_BASE_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+    public static final String GeoDB_API_KEY = BuildConfig.GeoDB_API_KEY;
+}
