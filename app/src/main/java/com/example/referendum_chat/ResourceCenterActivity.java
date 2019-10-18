5205547ae59c69eb4af8954da8e41b8f054db041
Robert Muthuri <muthuri.r@gmail.com>
2019-10-18 15:25:44 +0300

    Adds test views to display resource center details to resource center layout.

--- app/src/main/java/com/example/referendum_chat/ResourceCenterActivity.java
+++ app/src/main/java/com/example/referendum_chat/ResourceCenterActivity.java
@@ -8,8 +8,12 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -21,7 +25,10 @@ import retrofit2.Response;
 
 public class ResourceCenterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @BindView(R.id.resourceSpinner) Spinner mResourceSpinner;
-    @BindView(R.id.kenyanCitiesListView) ListView mKenyanCitiesListView;
+//    @BindView(R.id.kenyanCitiesListView) ListView mKenyanCitiesListView;
+    @BindView(R.id.centerImageView) ImageView mCenterImageView;
+    @BindView(R.id.centerTextView) TextView mCenterTextView;
+    @BindView(R.id.centerLocationTextView) TextView mCenterLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,8 +77,17 @@ public class ResourceCenterActivity extends AppCompatActivity implements Adapter
     // Define the selection event handler for the spinner with the corresponding callback method.
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
         // An item was selected, you can retrieve the selected item using
-        parent.getItemAtPosition(pos);
-        Log.e("Item Selected", "onItemSelected: " + parent.getItemAtPosition(pos));
+//        parent.getItemAtPosition(pos);
+//        Log.e("Item Selected", "onItemSelected: " + parent.getItemAtPosition(pos));
+        String city = parent.getItemAtPosition(pos).toString();
+        Log.e("Item Selected", "onItemSelected: " + city);
+        if (city.equals("Nairobi")) {
+            Picasso.get().load(R.drawable.cipit_logo).into(mCenterImageView);
+            Log.e("Image tagged", "image set: " + mCenterImageView);
+            mCenterTextView.setText("Cipit");
+            Log.e("text set", "text set: " + mCenterTextView);
+
+        }
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
