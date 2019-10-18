d1633fe61def111976c0e9f2f333c0577c3b38e1
Robert Muthuri <muthuri.r@gmail.com>
2019-10-18 11:21:23 +0300

    Detects user spinner selection by implementing the on-item-selected-listener and the methods onItemSelected, and onNothingSelected.

--- app/src/main/java/com/example/referendum_chat/ResourceCenterActivity.java
+++ app/src/main/java/com/example/referendum_chat/ResourceCenterActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -16,7 +19,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ResourceCenterActivity extends AppCompatActivity {
+public class ResourceCenterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @BindView(R.id.resourceSpinner) Spinner mResourceSpinner;
     @BindView(R.id.kenyanCitiesListView) ListView mKenyanCitiesListView;
 
@@ -32,7 +35,8 @@ public class ResourceCenterActivity extends AppCompatActivity {
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-//        mResourceSpinner.setAdapter(adapter);
+        // mResourceSpinner.setAdapter(adapter);
+        mResourceSpinner.setOnItemSelectedListener(this);
 
         GeoDBApiInterface client = GeoDBClient.getClient();
         String countryIds = "ke";
@@ -63,5 +67,14 @@ public class ResourceCenterActivity extends AppCompatActivity {
             }
         });
     }
+    // Define the selection event handler for the spinner with the corresponding callback method.
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // An item was selected, you can retrieve the selected item using
+        parent.getItemAtPosition(pos);
+        Log.e("Item Selected", "onItemSelected: " + parent.getItemAtPosition(pos));
+    }
 
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback.
+    }
 }
