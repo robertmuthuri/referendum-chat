a56228e8a21e217b2743eb3aec5e9910cca4da31
Robert Muthuri <muthuri.r@gmail.com>
2019-10-30 00:49:42 +0300

    Adds a login activity and respective layout.

---
+++ app/src/main/java/com/example/referendum_chat/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.referendum_chat.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.referendum_chat.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
